89a7211842783a03c20048115c700e8e79f3cd6c
Divider <89965004+it-cio@users.noreply.github.com>
2021-09-29 23:10:55 +0300

    create dev branch

--- forecast.py
+++ forecast.py
@@ -1,16 +1,30 @@
-import requests
+import aiohttp
+import asyncio
+
+async def main():
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://python.org') as response:
+
+            print("Status:", response.status)
+            print("Content-type:", response.headers['content-type'])
+
+            html = await response.text()
+            print("Body:", html[:15], "...")
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
 
 
-def weather_request():
-    url = 'https://wttr.in/Sochi'
-    weather_parameters = {
-        'format': 2,
-        '0': '',
-        'T': '',
-        'M': '',
-        'lang': 'ru'
-    }
-    forecast = f'Weather forecast: {requests.get(url, params=weather_parameters).text}'
-    print(forecast)
-    return forecast
-
+# def weather_request():
+#     url = 'https://wttr.in/Sochi'
+#     weather_parameters = {
+#         'format': 2,
+#         '0': '',
+#         'T': '',
+#         'M': '',
+#         'lang': 'ru'
+#     }
+#     forecast = f'Weather forecast: {requests.get(url, params=weather_parameters).text}'
+#     print(forecast)
+#     return forecast
